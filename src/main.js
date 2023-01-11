0977eabb4d1ad64f8f3465b50b5df66ee4621a2d
smartdev328 <solid.dev3028@gmail.com>
2023-01-11 09:08:52 +0100

    Put sign in on App.vue

--- src/main.js
+++ src/main.js
@@ -4,7 +4,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './assets/css/app.min.css'
+import './assets/css/bootstrap.min.css'
+import './assets/css/icons.min.css'
+import './assets/css/custom.min.css'
+
+import './assets/js/layout.js'
+import './assets/js/bootstrap.bundle.min.js'
 
 const app = createApp(App)
 
