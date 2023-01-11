8894914d25b3fa65fc538d97bbd30740f5c2b63d
smartdev328 <solid.dev3028@gmail.com>
2023-01-11 11:53:12 +0100

    All I need to do was change the order of the css load. mschew.

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/css/app.min.css'
 import './assets/css/bootstrap.min.css'
 import './assets/css/icons.min.css'
+import './assets/css/app.min.css'
 import './assets/css/custom.min.css'
 
 import './assets/js/layout.js'
